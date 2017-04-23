7ee962849517cf82dab00b1d55eaebe0bafdc344
Madhen Kuppuswamy Venkataraman <madhen.kuppuswamy.venkataraman@macys.com>
2017-04-23 00:24:52 -0700

    Initial Commit

---
+++ APImirror/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APImirror
+//
+//  Created by Madhen Kuppuswamy Venkataraman on 4/23/17.
+//  Copyright © 2017 Madhen Kuppuswamy Venkataraman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
